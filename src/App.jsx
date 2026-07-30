84eb14cf588395a456e134c49bd61d838d657f90
Fatemeh Hasanpour <hpfatemeh35@gmail.com>
2026-07-30 12:41:00 +0330

    loading questions from fake api

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
+import { createContext, useEffect, useReducer } from "react";
 import Quiz from "./components/Quiz";
 import StartScreen from "./components/StartScreen";
+import Spinner from "./components/Spinner";
+import Error from "./components/Error";
+import QuestionBox from "./components/QuestionBox";
+
+export const AppContext = createContext();
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecived": {
+      return { ...state, questions: action.payload, status: "ready" };
+    }
+    case "dataFeild": {
+      return { ...state, status: "error" };
+    }
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { status, questions } = state;
+
+  useEffect(function () {
+    async function getData() {
+      try {
+        const response = await fetch("http://localhost:3000/questions");
+        if (!response.ok) {
+          throw new Error("Something went wrong while loading the questions.");
+        }
+        const data = await response.json();
+        dispatch({ type: "dataRecived", payload: data });
+      } catch (e) {
+        dispatch({ type: "dataFeild" });
+      }
+    }
+    getData();
+  }, []);
+
   return (
-    <div className="app">
-      <StartScreen />
-    </div>
+    <AppContext.Provider value={{ questions, dispatch }}>
+      <div className="app">
+        {status === "ready" && <QuestionBox />}
+        {status === "loading" && <Spinner />}
+        {status === "error" && <Error />}
+      </div>
+    </AppContext.Provider>
   );
 }
---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="error">
+      <span>⛔</span>
+      <p>Something went wrong while loading the questions.</p>
+    </div>
+  );
+};
+
+export default Error;
